6d4683fa4c70c9b6649b7be615eb0fda7744a74c
Vaughn Dickson <vaughnd@gmail.com>
2014-02-21 16:14:54 +0200

    Add with-logging-context macro for MDC and log/spy

--- src/clojure_example_logback_integration/log.clj
+++ src/clojure_example_logback_integration/log.clj
@@ -1,31 +1,51 @@
 (ns clojure-example-logback-integration.log
-  (:require [clojure.tools.logging :as log]
-            [clojure.tools.logging.impl :as logimpl]))
+  (:require [clojure.pprint :as pprint])
+  (:import [ch.qos.logback.classic Level Logger]
+           [java.io StringWriter]
+           [org.slf4j LoggerFactory MDC]))
+
+(def logger ^ch.qos.logback.classic.Logger (LoggerFactory/getLogger "clojure-example-logback-integration"))
 
 (defn set-log-level!
   "Pass keyword :error :info :debug"
   [level]
-  (let [logger (logimpl/get-logger log/*logger-factory* "clojure-example-logback-integration")]
-    (case level
-      :debug (.setLevel logger ch.qos.logback.classic.Level/DEBUG)
-      :info (.setLevel logger ch.qos.logback.classic.Level/INFO)
-      :error (.setLevel logger ch.qos.logback.classic.Level/ERROR))))
+  (case level
+    :debug (.setLevel logger Level/DEBUG)
+    :info (.setLevel logger Level/INFO)
+    :error (.setLevel logger Level/ERROR)))
 
-(defmacro debug [& args]
-  `(log/debug (str ~@args)))
+(defmacro with-logging-context [context & body]
+  "Use this to add a map to any logging wrapped in the macro. Macro can be nested.
+  (with-logging-context {:key \"value\"} (log/info \"yay\"))
+  "
+  `(let [wrapped-context# ~context
+         ctx# (MDC/getCopyOfContextMap)]
+     (try
+       (if (map? wrapped-context#)
+         (doall (map (fn [[k# v#]] (MDC/put (name k#) (str v#))) wrapped-context#)))
+       ~@body
+       (finally
+         (if ctx#
+           (MDC/setContextMap ctx#)
+           (MDC/clear))))))
 
-(defmacro info [& args]
-  `(log/info (str ~@args)))
+(defmacro debug [& msg]
+  `(.debug logger (print-str ~@msg)))
 
-(defmacro warn [e & args]
-  `(log/warn (str ~@args) ~e))
+(defmacro info [& msg]
+  `(.info logger (print-str ~@msg)))
 
-(defmacro warn [& args]
-  `(log/warn (str ~@args)))
+(defmacro error [throwable & msg]
+  `(if (instance? Throwable ~throwable)
+    (.error logger (print-str ~@msg) ~throwable)
+    (.error logger (print-str ~throwable ~@msg))))
 
-(defmacro error [& args]
-  `(log/error (str ~@args)))
-
-(defmacro error [e & args]
-  `(log/error ~e (str ~@args)))
-
+(defmacro spy
+  [expr]
+  `(let [a# ~expr
+         w# (StringWriter.)]
+     (pprint/pprint '~expr w#)
+     (.append w# " => ")
+     (pprint/pprint a# w#)
+     (error (.toString w#))
+     a#))
